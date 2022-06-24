7255ffa78d2ab112c49bb35c3cf0b2ca53d6a2d1
Ruiyot Abby <ruiyotabby@gmail.com>
2022-06-24 07:09:28 +0300

    creating custom user models

--- leave/models.py
+++ leave/models.py
@@ -2,6 +2,38 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
 
+
+class UserManager(BaseUserManager):
+    def create_user(self, pjnumber, password=None):
+        if not pjnumber:
+            raise ValueError('Users must have pj numbers')
+        user = self.model(
+            pjnumber=pjnumber,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, pjnumber, password):
+        user = self.create_user(
+            pjnumber, passwword=password
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, pjnumber, password):
+        user = self.create_user(
+            pjnumber,
+            password=password
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractBaseUser):
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=50)
@@ -10,7 +42,7 @@ class User(AbstractBaseUser):
     designation = models.CharField(max_length=50)
     station = models.CharField(max_length=50)
     section = models.CharField(max_length=50)
-    name = fname + '' + lname
+    # name = fname + '' + lname
 
     is_active = models.BooleanField(default=True)
     staff = models.BooleanField(default=False) #admin user
@@ -21,13 +53,13 @@ class User(AbstractBaseUser):
 
     def get_full_name(self):
         #user is identified by name
-        return self.name
+        return self.pjnumber
     
     def __str__(self):
-        return self.name
+        return self.pjnumber
 
     def has_perm(self, perm, obj=None):
-        "Does User have user specific permissions?"#Yes, always
+        """Does User have user specific permissions?"""#Yes, always
         return True
 
     def has_module_perm(self, perm, app_label):
@@ -40,3 +72,10 @@ class User(AbstractBaseUser):
     @property
     def is_admin(self):
         return self.admin
+
+    object = UserManager()
+
+    """ 
+    from django.contrib.auth import get_user_model
+    user = get_user_model()
+    """
--- lms/settings.py
+++ lms/settings.py
@@ -86,19 +86,21 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
+
+AUTH_USER_MODEL = 'leave.User'
 
 
 # Internationalization
